e04d79f17b4216f14abeed49df4357f0f0000ea9
ade.hafis <ade.hafis@socialbread.id>
2023-09-19 17:22:58 +0700

    Update data

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -124,17 +124,7 @@
 
                     <div class="container-fluid flex-grow-1 container-p-y">
                         <!-- Layout Demo -->
-                        <div class="layout-demo-wrapper">
-                            <div class="layout-demo-placeholder">
-                                <img src="../assets/img/layouts/layout-fluid-light.png" class="img-fluid"
-                                    alt="Layout fluid" data-app-light-img="layouts/layout-fluid-light.png"
-                                    data-app-dark-img="layouts/layout-fluid-dark.png" />
-                            </div>
-                            <div class="layout-demo-info">
-                                <h4>Layout fluid</h4>
-                                <p>Fluid layout sets a <code>100% width</code> at each responsive breakpoint.</p>
-                            </div>
-                        </div>
+                        @yield('page-content')
                         <!--/ Layout Demo -->
                     </div>
                     <!-- / Content -->
